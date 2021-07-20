6eb9260077379b530d421b4af76cfcf3893f7658
Deepak64bit <deepak_b190403cs@nitc.ac.in>
2021-07-20 12:22:09 +0530

    initial implementation of db

---
+++ todo/db.py
@@ -0,0 +1,45 @@
+import datetime
+import random
+import sqlite3
+
+import click 
+from flask import current_app, g
+from flask.cli import with_appcontext
+
+from faker import Faker
+
+
+def get_db():
+    if 'db' not in g: 
+        dbname = current_app.config['DATABASE'] 
+        g.db = sqlite3.connect(dbname)
+        g.db.execute("PRAGMA foreign_keys = ON;")
+    return g.db
+
+
+def close_db(e=None):
+    db = g.pop('db', None)
+    if db is not None:
+        db.close()
+
+def init_db():
+    db = get_db()
+    #tables are created
+    f = current_app.open_resource("sql/create.sql")
+    sql_codes = f.read().decode("ascii")
+    cur = db.cursor()
+    cur.executescript(sql_codes)
+    cur.close()
+    db.commit()
+    close_db()
+
+
+@click.command('initdb', help="initialise the database")
+@with_appcontext
+def init_db_command():
+    init_db()
+    click.echo('DB initialised') 
+
+def init_app(app):
+    app.teardown_appcontext(close_db)
+    app.cli.add_command(init_db_command)
